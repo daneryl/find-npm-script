bda0aa45d051fe2795523d35b5840394c09bf243
Daneryl <daneryl@gmail.com>
2018-03-29 20:35:53 +0200

    added function as a widget

--- init.zsh
+++ init.zsh
@@ -1,12 +1,14 @@
-function find_npm_scripts {
+function _find_npm_scripts {
   if [ ! -f package.json ]; then
     echo "package.json not found" >&2
-    return 1
+    zle send-break
   fi
   output=$(jq '.scripts | keys[]' package.json -r | fzf --height 30% --reverse --bind 'shift-tab:up,tab:down' --cycle)
   if [ -z "$output" ]; then
-    return 0
+    zle accept-line
   else
     yarn $output
   fi
 }
+
+zle -N find-npm-scripts _find_npm_scripts
